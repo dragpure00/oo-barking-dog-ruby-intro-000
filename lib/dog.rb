0acecfcc6692b1e39faa3a416467b5aa3116616c
Ayaz Araf <ayaz.araf@gmail.com>
2020-07-24 00:53:13 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,10 +6,10 @@ class Dog
   def name
     @name
   end
-      def initialize(bark)
+      def initialize(bark="woof!")
         @bark=bark
       end
-      def bark=(bark="woof!")
+      def bark=(bark)
         @bark=bark
       end
       def bark
